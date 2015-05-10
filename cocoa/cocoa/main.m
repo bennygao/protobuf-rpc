e9d8e21d16f411b250837f9b43f36c799b199dac
bennygao <bennygao@yingshibao>
2015-05-10 21:28:03 +0800

    修改objc sample代码。

--- cocoa/cocoa/main.m
+++ cocoa/cocoa/main.m
@@ -7,11 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <ProtocolBuffers/TextFormat.h>
+#import "Yingshibao.pb.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        UserInfoBuilder* builder = [UserInfo builder];
+        builder.schoolName = @"北京大学";
+        builder.nickName = @"马大哈";
+        builder.phone = @"13810422191";
+        builder.examType = 1;
+        builder.channelName = @"360应用商店";
+        UserInfo* userInfo = [builder build];
+        int size = [userInfo serializedSize];
+        NSLog(@"serializedSize=%d, %@", size, userInfo);
+        
+        uint8_t buffer[size];
+        NSOutputStream* ostream = [[NSOutputStream alloc] initToBuffer:buffer capacity:size];
+        [ostream open];
+        [userInfo writeToOutputStream:ostream];
+        [ostream close];
+        
+        NSData *data = [NSData dataWithBytes: buffer length: size];
+        NSInputStream* istream = [[NSInputStream alloc] initWithData:data];
+        [istream open];
+        UserInfo *another = [UserInfo parseFromData:data];
+        NSLog(@"serializedSize=%d, %@", size, another);
+        
     }
     return 0;
 }
