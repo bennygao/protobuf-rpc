4644ef797a08308fdc8a8d94971b38cbe7d6ad70
bennygao <bennygao@yingshibao.com>
2015-05-25 23:21:59 +0800

    修改windows用的pbrpcc.bat

--- bin/pbrpcc.bat
+++ bin/pbrpcc.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 
+setlocal EnableDelayedExpansion
 SET BINDIR=%~dp0
 SET BATNAME=%0
-SET FLAG=""
-SET TARGET=""
-SET OUTDIR=""
-SET PROTO=""
+
 
 :Loop
 IF "%1"=="" GOTO Continue
@@ -13,8 +11,6 @@ IF "%1"=="" GOTO Continue
 		SET FLAG="-g"
 	) ELSE IF "%1"=="-o" (
 		SET FLAG="-o"
-	) ELSE IF "%1"=="-p" (
-		SET FLAG="-p"
 	) ELSE IF "%1"=="-h" (
 		GOTO usage
 	) ELSE (
@@ -24,17 +20,17 @@ IF "%1"=="" GOTO Continue
 		) ELSE IF %FLAG%=="-o" (
 			SET OUTDIR=%1
 			SET FLAG=""
-		) ELSE IF %FLAG%=="-p" (
-			SET PROTO=%1
-			SET FLAG=""
 		) ELSE (
-			GOTO Usage
+			IF %TARGET%=="" (GOTO Usage)
+			IF %OUTDIR%=="" (GOTO Usage)
+			GOTO Continue
 		)
 	)
 SHIFT
 GOTO Loop
 :Continue
 
+SHIFT /4
 IF NOT "%TARGET%"=="java" (
 	IF NOT "%TARGET%"=="objc" (
 		IF NOT "%TARGET%"=="html" (
@@ -43,15 +39,16 @@ IF NOT "%TARGET%"=="java" (
 	)
 )
 
-IF %PROTO%=="" (GOTO Usage)
 IF %OUTDIR%=="" (GOTO Usage)
 
 SET GENRPC_ARGS=%TARGET% %OUTDIR%
-ECHO %GENRPC_ARGS%
+REM ECHO %GENRPC_ARGS%
 IF "%TARGET%"=="java" (
-	%BINDIR%\protoc.exe --java_out=%OUTDIR% --plugin=protoc-gen-rpc=%BINDIR%\protoc-gen-rpc.bat --rpc_out=%OUTDIR% %PROTO%
+	%BINDIR%\protoc.exe --java_out=%OUTDIR% --plugin=protoc-gen-rpc=%BINDIR%\protoc-gen-rpc.bat --rpc_out=%OUTDIR% %1 %2 %3 %4 %5 %6 %7 %8 %9
 ) ELSE IF "%TARGET%"=="objc" (
 	ECHO "Don't support generate objective-c source on Windows."
+) ELSE IF "%TARGET%"=="html" (
+	%BINDIR%\protoc.exe --plugin=protoc-gen-rpc=%BINDIR%\protoc-gen-rpc.bat --rpc_out=%OUTDIR% %1 %2 %3 %4 %5 %6 %7 %8 %9
 )
 
 GOTO end
--- client/src/main/java/com/yingshibao/app/client/Client.java
+++ client/src/main/java/com/yingshibao/app/client/Client.java
@@ -1,5 +1,6 @@
 package com.yingshibao.app.client;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.yingshibao.app.idl.*;
@@ -16,44 +17,56 @@ import cc.devfun.pbrpc.nio.NioClientSession;
 public class Client {
 	public static void main(String[] args) throws Exception {
 		Client client = new Client("localhost", 10000);
-		for (int i = 0; i < 100; ++i) {
-			client.testUnregisteredService();
-			client.testRegisteredService();
-
-		}
+		client.doTest();
 		client.stop();
 	}
 
+
+    final static int LOOP_COUNT = 100;
 	private NioClientEndpoint endpoint;
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	private AtomicInteger counter = new AtomicInteger(0);
+    String remoteAddr;
+    int remotePort;
+    CountDownLatch latch;
 
 	public Client(String addr, int port) throws Exception {
+        this.remoteAddr = addr;
+        this.remotePort = port;
+
 		endpoint = new NioClientEndpoint();
 		endpoint.registerService(new Push(new PushImpl()));
 		endpoint.registerService(new UserManager());
-		endpoint.connect(addr, port);
-		endpoint.start();
+
 	}
+
+    public void doTest() throws Exception {
+        endpoint.connect(remoteAddr, remotePort);
+        endpoint.start();
+
+        latch = new CountDownLatch(2 * LOOP_COUNT);
+        for (int i = 0; i < LOOP_COUNT; ++i) {
+            testUnregisteredService();
+            testRegisteredService();
+        }
+    }
 
 	public void testRegisteredService() throws Exception {
 		UserManager.Client client = new UserManager.Client(new NioClientSession(endpoint));
 		UserInfo userInfo = UserInfo.newBuilder().setChannelName("360应用商店")
 				.setPhone("13810773316").setExamType(1).setNickName("Johnn")
 				.build();
-		
-		counter.incrementAndGet();
+
 		client.registerNewUser(userInfo, new Endpoint.Callback() {
 			@Override
 			public void onResponse(GeneratedMessage response) {
 				RegisterResult result = (RegisterResult) response;
 				logger.info("ASYNC:注册用户返回:" + TextFormat.printToUnicodeString(result));
-				counter.decrementAndGet();
+                latch.countDown();
 			}
 
 			@Override
 			public void onError(Endpoint.RpcError error) {
-                counter.decrementAndGet();
+                latch.countDown();
                 if (error == Endpoint.RpcError.rpc_canceled) {
                     logger.error("ASYNC:RPC调用被取消");
                 } else if (error == Endpoint.RpcError.service_not_exist) {
@@ -84,17 +97,17 @@ public class Client {
 		CourseType courseType = CourseType.newBuilder().setNum(10).setPageNum(1).setCourseType(1).build();
 		CourseManager.Client client = new CourseManager.Client(new NioClientSession(endpoint));
 
-		counter.incrementAndGet();
 		client.getCourseList(courseType, new Endpoint.Callback() {
 			@Override
 			public void onResponse(GeneratedMessage response) {
+                latch.countDown();
 				CourseList courseList = (CourseList) response;
 				logger.error("SYNC:注册用户返回:" + TextFormat.printToUnicodeString(courseList));
 			}
 
 			@Override
 			public void onError(Endpoint.RpcError error) {
-				counter.decrementAndGet();
+                latch.countDown();
 				if (error == Endpoint.RpcError.rpc_canceled) {
 					logger.error("ASYNC:RPC调用被取消");
 				} else if (error == Endpoint.RpcError.service_not_exist) {
@@ -110,10 +123,7 @@ public class Client {
 	}
 	
 	public void stop() throws Exception {
-		while (counter.get() > 0) {
-			Thread.sleep(1000L);
-		}
-		
+		latch.await();
 		endpoint.stop();
 	}
 }
